01062a12160444bf535d2d0917738bb10113ee90
AnjaliVed <144322185+AnjaliVed@users.noreply.github.com>
2023-09-07 13:58:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+const songs = [
+      {
+        name: 'Tere Naino Mein',
+        path: 'music/song 1 naino.mp3',
+        artist: 'The Bilz And Kashif',
+        
+      },
+      {
+        name: 'Tera Fitoor',
+        path: 'music/song 2 tera fitoor.mp3',
+        artist: 'Arijit Singh',
+        
+      },
+      {
+        name: 'Tera Deedar Hua',
+        path: 'music/song 3 tera deedar.mp3',
+        artist: 'Rahat Fateh Ali Khan',
+        
+      },
+      {
+        name: 'Ishq Wala Love',
+        path: 'music/song 4 ishq love.mp3',
+        artist: ' Neeti Mohan, Shekhar Ravjiani, Salim Merchant',
+        
+      },
+      {
+        name: 'Ishq Hua',
+        path: 'music/song 5 ishq hua.mp3',
+        artist: 'Shreya Goshal, Sonu Nigam',
+        
+      }
+];
+
